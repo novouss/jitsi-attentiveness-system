2b57b63458d960d1bee025af39de88052d4e8f70
Raymond Gorospe <raymondgorospe1113@gmail.com>
2023-03-19 00:24:41 +0800

    Added Notification Functions and Properties

--- interface/static/js/client_configuration.js
+++ interface/static/js/client_configuration.js
@@ -11,96 +11,8 @@ function initJitsi() {
     api_jitsi = new JitsiMeetExternalAPI(domain, options);
 
     var height_window = $(window).height();
-    $("#div_container_of_body_elements").height(height_window);
-    $("#div_iframe_jitsi").height(height_window);
-}
-
-function init_jitsi_config() {
-    
-    disable_debug_panel()
-    initJitsi()
-
-    $("#btn_start_comparing").click(function() {
-        // Enable user Webcam
-        enable_floating_webcam();
-        
-        // The captureWebcam function loads before the webcam could be enable.
-        // Wait for 2 seconds to make sure video is loaded.
-        setTimeout(function() {
-            // Run sign_recognition
-            start_capturing_and_requesting = true;
-            captureWebcam('sign_classification');
-        }, 2000);
-
-    })
-    
-    $("#btn_start_rand_comparing").click(function() {
-        // Enable user Webcam
-        enable_floating_webcam();
-
-        // Wait for 2 seconds to make sure video is loaded.
-        setTimeout(function() {
-            // Run rand_sign_classification
-            start_capturing_and_requesting = true;
-            class_number = Math.floor(Math.random() * 10);
-            send_sign_label(class_number);
-            captureWebcam('rand_sign_classification');
-        }, 2000);
-
-    })
-
-    $("#btn_stop_comparing").click(function() {
-        reset_sign_classification();
-        start_capturing_and_requesting = false;
-        disable_floating_webcam();
-    })    
-    
-    $("#btn_stop_rand_comparing").click(function() {
-        reset_rand_sign_classification();
-        start_capturing_and_requesting = false;
-        disable_floating_webcam();
-    })
-
-    // Webcam Properties
-    $("#webcam-popup").draggable();
-    $('#close-webcam-popup').click(function() {
-        reset_sign_classification();
-        reset_rand_sign_classification();
-        disable_floating_webcam();
-    });
-}
-
-function enable_floating_webcam() {
-    $('#webcam-popup').prop("hidden", false)
-    $('#webcam-popup').show();
-
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-        // Display the webcam stream on the canvas
-        var video = document.getElementById('webcam-video');
-        video.srcObject = stream;
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
-    })
-    .catch(function(err) {
-        console.log("Error accessing webcam: " + err);
-    });
-}
-
-function disable_floating_webcam() {
-    $('#webcam-popup').prop("hidden", true)
-    $('#webcam-popup').hide();
-
-    const video = document.getElementById('webcam-video');
-    const mediaStream = video.srcObject;
-    const tracks = mediaStream.getTracks();
-
-    tracks.forEach(function(track) {
-        track.stop();
-    });
-
-    video.srcObject = null;
+    $('#div_container_of_body_elements').height(height_window);
+    $('#div_iframe_jitsi').height(height_window);
 }
 
 function captureWebcam(application) {
@@ -122,19 +34,114 @@ function getWebcamDataURL() {
         return null;
     }
 
-    const canvas = document.createElement('canvas')
+    const canvas = document.createElement('canvas');
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0)
+    canvas.getContext('2d').drawImage(video, 0, 0);
 
-    return canvas.toDataURL();
+    return canvas.toDataURL();   
+}
+
+function init_config() {
     
+    disable_debug_panel();
+    hide_notification();
+    initJitsi();
+
+    $('#btn_start_comparing').click(function() {
+        // Enable user Webcam
+        enable_floating_webcam();
+        
+        // The captureWebcam function loads before the webcam could be enable.
+        // Wait for 2 seconds to make sure video is loaded.
+        setTimeout(function() {
+            // Run sign_recognition
+            start_capturing_and_requesting = true;
+            captureWebcam('sign_classification');
+        }, WEBCAM_WAIT_TIME);
+
+    })
+    
+    $('#btn_start_rand_comparing').click(function() {
+        // Enable user Webcam
+        enable_floating_webcam();
+
+        // Wait for 2 seconds to make sure video is loaded.
+        setTimeout(function() {
+            // Run rand_sign_classification
+            start_capturing_and_requesting = true;
+            class_number = Math.floor(Math.random() * 10);
+            send_sign_label(class_number);
+            captureWebcam('rand_sign_classification');
+        }, WEBCAM_WAIT_TIME);
+
+    })
+
+    $('#btn_stop_comparing').click(function() {
+        reset_sign_classification();
+        start_capturing_and_requesting = false;
+        disable_floating_webcam();
+    })    
+    
+    $('#btn_stop_rand_comparing').click(function() {
+        reset_rand_sign_classification();
+        start_capturing_and_requesting = false;
+        disable_floating_webcam();
+    })
+
+    // Webcam Properties
+    $('#webcam-popup').draggable();
+    $('#close-webcam-popup').click(function() {
+        reset_sign_classification();
+        reset_rand_sign_classification();
+        disable_floating_webcam();
+    });
+}
+
+function show_notification(text) {
+    // TODO: Add a highlighting text indicator for show_notification
+    $('.notification-container').show();
+    $('.notification-text').text(text);
+}
+
+function hide_notification() {
+    $('.notification-container').hide();
+}
+
+function enable_floating_webcam() {
+    // FIXME: There's a bug that the draggable webcam does not update upon resize, or has no maximum bounding box.
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+        // Display the webcam stream on the canvas
+        var video = document.getElementById('webcam-video');
+        video.srcObject = stream;
+        video.onloadedmetadata = function(e) {
+            video.play();
+        };
+    })
+    .catch(function(err) {
+        console.log("Error accessing webcam: " + err);
+    });
+}
+
+function disable_floating_webcam() {
+    $('#webcam-popup').hide();
+
+    const video = document.getElementById('webcam-video');
+    const mediaStream = video.srcObject;
+    const tracks = mediaStream.getTracks();
+
+    tracks.forEach(function(track) {
+        track.stop();
+    });
+
+    video.srcObject = null;
 }
 
 function enable_debug_panel(){
-    $("#debugging_panel").prop("hidden", false)
+    $('#debugging_panel').prop('hidden', false)
 } 
 
 function disable_debug_panel(){
-    $("#debugging_panel").prop("hidden", true)
+    $('#debugging_panel').prop('hidden', true)
 }
--- interface/static/js/constants.js
+++ interface/static/js/constants.js
@@ -1,2 +1,3 @@
 const API_BASE_URL = "http://127.0.0.1:5000/";
 const REQUESTS_TIMEOUT = 600000; 
+const WEBCAM_WAIT_TIME = 2000; // in milliseconds
