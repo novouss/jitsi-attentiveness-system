46322166ca6a34f1ada57b42778892765f4b0ee9
Raymond Gorospe <raymondgorospe1113@gmail.com>
2023-03-20 20:35:52 +0800

    Removed deprecated functions

--- interface/static/js/client_requests.js
+++ interface/static/js/client_requests.js
@@ -1,58 +1,65 @@
 
 function send_sign_label(number) {
-
-    let form_data = new FormData();
-    form_data.append("number", number);
-
-    $.ajax({
-        type: "POST",
-        enctype: "multipart/form-data",
-        url: API_BASE_URL + "get_sign_label",
-        data: form_data,
-        processData: false,
-        contentType: false,
-        cache: false,
-        timeout: REQUESTS_TIMEOUT,
-        complete: function(xhr) {
-            let status = xhr.status;
-            let response = xhr.responseJSON;
-            let error_message;
-            switch (status) {
-                case 200:
-                    return response['sign_label'];
-                default:
-                    console.log(response['log']);
-                    return null;
-            }      
-        }
-    });
+    return new Promise(function(resolve, reject) {
+        let form_data = new FormData();
+        form_data.append("number", number);
+    
+        $.ajax({
+            type: "POST",
+            enctype: "multipart/form-data",
+            url: API_BASE_URL + "get_sign_label",
+            data: form_data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: REQUESTS_TIMEOUT,
+            complete: function(xhr) {
+                let status = xhr.status;
+                let response = xhr.responseJSON;
+                let error_message;
+                switch (status) {
+                    case 200:
+                        resolve(response['sign_label']);
+                    case 408:
+                        reject(new Error("Request timed out"));
+                        break;
+                    default:
+                        reject(new Error("Request failed"));
+                }      
+            }
+        });
+    })
 }
 
 function send_sign_classification(dataFrame) {
+    return new Promise(function(resolve, reject) {
+        let form_data = new FormData();
+        form_data.append("dataURL_frame", dataFrame);
 
-    let form_data = new FormData();
-    form_data.append("dataURL_frame", dataFrame);
-
-    $.ajax({
-        type: "POST",
-        enctype: "multipart/form-data",
-        url: API_BASE_URL + "get_sign_classification",
-        data: form_data,
-        processData: false,
-        contentType: false,
-        cache: false,
-        timeout: REQUESTS_TIMEOUT,
-        complete: function(xhr) {
-            let status = xhr.status;
-            let response = xhr.responseJSON;
-            let error_message;
-            switch (status) {
-                case 200:
-                    return response;
-                default:
-                    console.log(response['log']);
-                    return null;
-            }      
-        }
-    });    
+        $.ajax({
+            type: "POST",
+            enctype: "multipart/form-data",
+            url: API_BASE_URL + "get_sign_classification",
+            data: form_data,
+            processData: false,
+            contentType: false,
+            cache: false,
+            timeout: REQUESTS_TIMEOUT,
+            complete: function(xhr) {
+                let status = xhr.status;
+                let response = xhr.responseJSON;
+                let error_message;
+                switch (status) {
+                    case 200:
+                        resolve(response);
+                        break;
+                    case 408:
+                        reject(new Error("Request timed out"));
+                        break;
+                    default:
+                        reject(new Error("Request failed"));
+                }      
+            }
+        });
+    });
 }
